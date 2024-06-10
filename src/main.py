6e7ed3446243b2b4a609b7a17ec829d199d298ba
Christian Aucane <christianaucane1@gmail.com>
2024-06-10 14:05:24 +0200

    Code lint

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 from fractales import SierpinskiTriangle
 
-def sierpinski_triangle(max_order=5):
+
+def sierpinski_triangle(min_order=0, max_order=5):
     fig, axs = plt.subplots(1, max_order + 1, figsize=(15, 5))
-    for order in range(0, max_order + 1):
+    for order in range(min_order, max_order + 1):
         sierpinski = SierpinskiTriangle(order=order)
         sierpinski.plot(axs[order])
     plt.tight_layout()
@@ -14,8 +15,7 @@ def sierpinski_triangle(max_order=5):
 
 
 def main():
-
-    sierpinski_triangle(max_order=5)
+    sierpinski_triangle()
 
 
 if __name__ == "__main__":
