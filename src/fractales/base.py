b0672cc7a16fc37469a7348533800e7358f72e9e
Christian Aucane <christianaucane1@gmail.com>
2024-06-11 17:35:16 +0200

    Add plotly in BaseFractales classes

--- src/fractales/base.py
+++ src/fractales/base.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 class BaseFractale:
@@ -12,16 +13,19 @@ class BaseFractale:
     def _generate_points(self) -> np.ndarray:
         raise NotImplementedError("Subclasses must implement this method")
     
-    def _specific_plot(self, ax: plt.Axes):
+    def _specific_matplotlib_plot(self, ax: plt.Axes):
         raise NotImplementedError("Subclasses must implement this method")
 
-    def plot(self, ax: plt.Axes | None = None, title: bool = True) -> plt.Figure:
+    def _specific_plotly_plot(self, fig: go.Figure):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def plot_matplotlib(self, ax: plt.Axes | None = None, title: bool = True, library: str = "matplotlib") -> plt.Figure:    
         if ax is None:
             fig, ax = plt.subplots()
         else:
             fig = ax.figure
 
-        self._specific_plot(ax)
+        self._specific_matplotlib_plot(ax)
         
         ax.set_aspect('equal')
         ax.axis('off')
@@ -29,9 +33,23 @@ class BaseFractale:
             ax.set_title(f"{self.title} ({self.order})")
         return fig
     
+    def plot_plotly(self, title: bool = True):
+        fig = go.Figure()
+        self._specific_plotly_plot(fig)
+        fig.update_layout(
+            xaxis_visible=False,
+            yaxis_visible=False,
+            showlegend=False
+            # width=self.width,
+            # height=self.height
+            
+        )
+        if title:
+            fig.update_layout(title_text=f"{self.title} ({self.order})")
+        return fig
 
 class BaseComplexFractale(BaseFractale):
-    def __init__(self, title: str, order: int, width: int = 800, height: int = 800, 
+    def __init__(self, title: str, max_iter: int = 100, width: int = 800, height: int = 800, 
                  x_min: float = -1, x_max: float = 1, y_min: float = -1, y_max: float = 1):
         self.width = width
         self.height = height
@@ -41,11 +59,8 @@ class BaseComplexFractale(BaseFractale):
         self.y_max = y_max
         self.x = np.linspace(self.x_min, self.x_max, self.width)
         self.y = np.linspace(self.y_min, self.y_max, self.height)
-        super().__init__(title, order)
-
-    @property
-    def max_iter(self):
-        return [1, 5, 10, 20, 50, 100][self.order]
+        self.max_iter = max_iter
+        super().__init__(title)
 
     def _generate_points(self) -> np.ndarray:
         z = np.zeros((self.height, self.width), dtype=np.complex128)
@@ -69,5 +84,8 @@ class BaseComplexFractale(BaseFractale):
     def _compute_z(self, z: np.ndarray, c: np.ndarray) -> np.ndarray:
         raise NotImplementedError("Subclasses must implement this method")
     
-    def _specific_plot(self, ax: plt.Axes):
+    def _specific_matplotlib_plot(self, ax: plt.Axes):
         ax.imshow(self.points)
+
+    def _specific_plotly_plot(self, fig: go.Figure):
+        fig.add_trace(go.Heatmap(z=self.points))
