4fa0e48845c2f3d199e18cbdc055604e545d83f7
Christian Aucane <christianaucane1@gmail.com>
2024-06-11 17:45:48 +0200

    Adapt run.sh to run streamlit server

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,14 +6,11 @@ if [ -d "venv" ]; then
     if [[ "$OS" == "Windows_NT" ]]; then
         # Activate virtual environment for Windows
         source venv/Scripts/activate
-        # Run the application with python
-        python src/main.py
     else
         # Activate virtual environment for Unix-based systems (Linux, macOS)
         source venv/bin/activate
-        # Run the application with python3
-        python3 src/main.py
     fi
+    streamlit run src/streamlit_app.py
 else
     echo "Virtual environment not found. Please run install.sh before running this script."
     exit 1
