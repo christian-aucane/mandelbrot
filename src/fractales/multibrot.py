be161f4a3d52a25fee5092a2a09617de43699e33
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-06-11 11:12:28 +0200

    update mandlebrot resolution and create mutlibrot.py

--- src/fractales/multibrot.py
+++ src/fractales/multibrot.py
@@ -1,9 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from .base import BaseFractale
 
-class Mandelbrot(BaseFractale):
-    def __init__(self, xmin=-2.0, xmax=0.5, ymin=-1.25, ymax=1.25, width=720, height=480, max_iter=None, title="Mandelbrot", order=3):
+
+class Multibrot(BaseFractale):
+    def __init__(self, power=3, xmin=-2.0, xmax=0.5, ymin=-1.25, ymax=1.25, width=720, height=480, max_iter=None, title="Multibrot", order=3):
+        self.power = power
         self.xmin = xmin
         self.xmax = xmax
         self.ymin = ymin
@@ -16,24 +17,19 @@ class Mandelbrot(BaseFractale):
     def iterations(self, order):
         return [1, 5, 10, 20, 50, 100][order]
 
-    def mandelbrot(self, c):
+    def multibrot(self, c):
         z = 0
         n = 0
         while abs(z) <= 2 and n < self.max_iter:
-            z = z*z + c
+            z = z**self.power + c
             n += 1
         return n
 
     def _generate_points(self):
         r1 = np.linspace(self.xmin, self.xmax, self.width)
         r2 = np.linspace(self.ymin, self.ymax, self.height)
-        return (r1, r2, np.array([[self.mandelbrot(complex(r, i)) for r in r1] for i in r2]))
+        return (r1, r2, np.array([[self.multibrot(complex(r, i)) for r in r1] for i in r2]))
 
     def _specific_plot(self, ax):
         d = self._generate_points()
         ax.imshow(d[2], extent=(self.xmin, self.xmax, self.ymin, self.ymax))
-
-if __name__ == "__main__":
-    mandelbrot = Mandelbrot(-2.0, 0.5, -1.25, 1.25, 1000, 1000, 5)
-    mandelbrot.plot()
-    plt.show()
--- src/main.py
+++ src/main.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 from fractales import SierpinskiTriangle
 from fractales.koch_snowflake import KochSnowflake
 from fractales.mandelbrot import Mandelbrot
+from fractales.multibrot import Multibrot
 
 
 def plot_all_fractales(fractale_classes, min_order=0, max_order=5):
     num_fractales = len(fractale_classes)
-    fig, axs = plt.subplots(num_fractales, max_order + 1, figsize=(15, 3 * num_fractales))
+    fig = plt.figure(figsize=(15, 3 * num_fractales))
     
     for i, fractale_class in enumerate(fractale_classes):
         for order in range(min_order, max_order + 1):
+            ax = fig.add_subplot(num_fractales, max_order + 1, i * (max_order + 1) + order + 1)
             fractale = fractale_class(order=order)
-            fractale.plot(axs[i, order])
-    plt.tight_layout()
+            fractale.plot(ax)
+    plt.subplots_adjust(hspace=0.5)
     plt.show()
 
 
@@ -21,7 +23,9 @@ def main():
     fractale_classes = [
         SierpinskiTriangle,
         KochSnowflake,
-        lambda order: Mandelbrot(order=order, max_iter=None)
+        lambda order: Mandelbrot(order=order, max_iter=None),
+        lambda order: Multibrot(power=order+2, order=order, max_iter=None),
+        lambda order: Multibrot(power=order+8, order=order, max_iter=None)
     ]
     # TODO : ajouter un argument pour le min_order et le max_order
 
